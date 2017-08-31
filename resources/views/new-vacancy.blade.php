6c6d635e134b564ffa545c07a3aa7881d4b16ba4
Taymison <taymisondeveloper@gmail.com>
2017-08-31 16:22:03 -0300

    css refatored

--- resources/views/new-vacancy.blade.php
+++ resources/views/new-vacancy.blade.php
@@ -19,16 +19,16 @@
                 </a>
             </div>
         </header>
-        <section class="new-vacancy">
+        <section class="main-section">
             <div class="wrapper wrapper--large-padding">
-                <h1 class="new-vacancy__title">New Vacancy</h1>
-                <form id="form-create-vacancy" action="{{ route('new-vacancy') }}" method="post">
+                <h1 class="main-section__title">New Vacancy</h1>
+                <form class="form" id="form-create-vacancy" action="{{ route('new-vacancy') }}" method="post">
                     {{ csrf_field() }}
                     @if (isset($companies))
-                        <h2 class="new-vacancy__subtitle">Step 1 - Select a company</h2>
-                        <div class="new-vacancy__companies-exist">
-                            <label class="new-vacancy__label" for="company">Select a company</label>
-                            <select class="new-vacancy__form-control" name="company_id" id="company" required>
+                        <h2 class="main-section__subtitle">Step 1 - Select a company</h2>
+                        <div class="main-section__companies-exist">
+                            <label class="form__label" for="company">Select a company</label>
+                            <select class="form__form-control" name="company_id" id="company" required>
                                 <option value="" selected>- No company selected -</option>
                                 @foreach ($companies as $company)
                                     <option value="{{ $company->id }}" @if ($company->id == old('company_id')) {{ 'selected' }} @endif>{{ $company->name }}</option>
@@ -38,36 +38,36 @@
                             <br>
                             <div class="row row--space-between">
                                 <div class="row__col">
-                                    <a href="{{ route('companies') }}" class="new-vacancy__form-control" type="button">Edit companies</a>
+                                    <a href="{{ route('companies') }}" class="form__form-control" type="button">Edit companies</a>
                                 </div>
                                 <div class="row__col">
-                                    <a href="{{ route('companies') }}" class="new-vacancy__form-control" type="button">Create a company</a>
+                                    <a href="{{ route('companies') }}" class="form__form-control" type="button">Create a company</a>
                                 </div>
                             </div>
                         </div>
                     @else
-                        <h2 class="new-vacancy__subtitle">Step 1 - Create a company</h2>
-                        <div class="new-vacancy__companies-not-exist">
-                            <a href="{{ route('companies') }}" class="new-vacancy__form-control" type="button">Create a company</a>
+                        <h2 class="main-section__subtitle">Step 1 - Create a company</h2>
+                        <div class="main-section__companies-not-exist">
+                            <a href="{{ route('companies') }}" class="form__form-control" type="button">Create a company</a>
                         </div>
                     @endif
                     <br>
                     <br>
                     <h2>Step 2 - Create a vacancy</h2>
-                    <label class="new-vacancy__label" for="vacancy-name">Name</label>
-                    <input class="new-vacancy__form-control" id="vacancy-name" type="text" name="name" placeholder="Name of vacancy" value="{{ old('name') }}" required>
+                    <label class="form__label" for="vacancy-name">Name</label>
+                    <input class="form__form-control" id="vacancy-name" type="text" name="name" placeholder="Name of vacancy" value="{{ old('name') }}" required>
                     @include('components.form-message-error', ['field' => 'name'])
                     <br>
-                    <label class="new-vacancy__label" for="vacancy-description">Description</label>
-                    <textarea class="new-vacancy__form-control" name="description" id="vacancy-description" cols="15" rows="5" placeholder="Short description of vacancy" required>{{ old('description') }}</textarea>
+                    <label class="form__label" for="vacancy-description">Description</label>
+                    <textarea class="form__form-control" name="description" id="vacancy-description" cols="15" rows="5" placeholder="Short description of vacancy" required>{{ old('description') }}</textarea>
                     @include('components.form-message-error', ['field' => 'description'])
                     <br>
-                    <label class="new-vacancy__label" for="vacancy-link">Link</label>
-                    <input class="new-vacancy__form-control" type="url" name="link" placeholder="https://www.vacancy.com" value="{{ old('link') }}" required>
+                    <label class="form__label" for="vacancy-link">Link</label>
+                    <input class="form__form-control" type="url" name="link" placeholder="https://www.vacancy.com" value="{{ old('link') }}" required>
                     @include('components.form-message-error', ['field' => 'link'])
                     <br>
-                    <label class="new-vacancy__label" for="vacancy-status">Status</label>
-                    <select class="new-vacancy__form-control" name="status" id="vacancy-status" required>
+                    <label class="form__label" for="vacancy-status">Status</label>
+                    <select class="form__form-control" name="status" id="vacancy-status" required>
                         @foreach ($possible_status as $status)
                             <option value="{{ $status }}" @if ($status == old('status')) {{ 'selected' }} @endif>{{ ucfirst($status) }}</option>
                         @endforeach
@@ -76,10 +76,10 @@
                     <br>
                     <div class="row row--space-between">
                         <div class="row__col">
-                            <a class="new-vacancy__form-control" type="button" href="/">Cancel</a>
+                            <a class="form__form-control" type="button" href="/">Cancel</a>
                         </div>
                         <div class="row__col">
-                            <input class="new-vacancy__form-control" type="submit" value="Register">
+                            <input class="form__form-control" type="submit" value="Register">
                         </div>
                     </div>
                 </form>
